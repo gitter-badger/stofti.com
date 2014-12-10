0cdeae052ae745a97406dc9960abc82fd94c2466
Wesley Song <wes.song@gmail.com>
2014-12-10 10:11:54 -0700

    start work on models, remove nav controller and files

--- public/angular/nav/navcontroller.js
+++
@@ -1,5 +0,0 @@
-var stoftiApp = angular.module('stoftiApp');
-
-stoftiApp.controller('NavController', ['$scope', function($scope){
-    $scope.navbar = {name: "navbar.html", url: "navbar.html"};
-}]);
