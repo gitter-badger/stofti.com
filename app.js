73f3c52f88ea918f168e3c26023fb964f5a99448
Wesley Song <wes.song@gmail.com>
2014-12-09 20:19:31 -0700

    Update app.js to use env variables for stormpath

--- app.js
+++ app.js
@@ -15,19 +15,14 @@ var indexController = require('./controllers/index.js');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-var stormpathMiddleware = stormpath.init(app, {
-    apiKeyFile: '/Users/wesleysong/IdeaProjects/stofti/.stormpath/apiKey.properties',
+app.use(stormpath.init(app, {
     application: 'https://api.stormpath.com/v1/applications/3smD8FEpyVekZTVxUoov2J',
-    secretKey: 'ow9hrboijwergojrboij4nhljrfgow8jhebodinrgh3o4jtobdjrh5jyp9iuwpr3jwelgjeo59jhoehf273grihgorjthorj5hr9jrth',
-    expandCustomData: true,
-    enableForgotPassword: true
-});
+    secretKey: '9p8helgibndbdlsirnfw84ehfeilurbn3l4gi8h3ir2hiweufhw789ehg98ehjrb3elhgk',
+}));
 
-app.use(stormpathMiddleware);
 app.use(express.static(__dirname + "/public"));
 
-app.get('/', function(request, response){
-    response.send("hello!");
-});
+app.get('/', indexController);
 
 app.listen(9000);
+console.log('Listening on port 9000');
