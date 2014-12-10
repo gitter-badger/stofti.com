d2df4a322fa06e9ae79b28989229f52bbd7f8452
Wesley Song <wes.song@gmail.com>
2014-12-10 09:22:40 -0700

    switched to angularstrap, removed bootstrap, changed navbar

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,5 @@
 var mongoose = require('mongoose');
 var Content = require("../models/content.js");
-var indexController = {
-    index: function(req, res) {
-        console.log('index page', req)
-    },
-    page: function(req, res) {
-        console.log('other page', req)
-    }
-};
+var indexController = {};
 
 module.exports = indexController;
--- public/angular/main/maincontroller.js
+++ public/angular/main/maincontroller.js
@@ -1,6 +1,26 @@
 var stoftiApp = angular.module('stoftiApp');
 
-stoftiApp.controller('MainController', ['$scope', '$rootScope', function($scope, $rootScope){
-    $scope.greeting = "Hello you crazy kids";
-    $scope.pageLocation = "home"
+stoftiApp.controller('MainController', ['$scope', '$state', function($scope, $state){
+    $scope.userMenu = [
+        {
+            "text": "Register",
+            "href": "/register",
+            "target": "_self"
+        },
+        {
+            "text": "Login",
+            "href": "/login",
+            "target": "_self"
+        },
+        {
+            "text": "Logout",
+            "href": "/logout",
+            "target": "_self"
+        }
+    ]
+    $scope.$watch(function($state){
+        console.log('Watching State', $state)
+    }, function(){
+
+    })
 }]);
