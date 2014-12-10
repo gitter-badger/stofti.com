0ac5e4374694c7cd2c40757ce3e94008c61919fb
Wesley Song <wes.song@gmail.com>
2014-12-10 09:22:24 -0700

    switched to angularstrap, removed bootstrap, changed navbar

--- public/angular/app.js
+++ public/angular/app.js
@@ -1,4 +1,24 @@
 var stoftiApp = angular.module('stoftiApp', [
     'ngResource',
-    'ngSanitize'
+    'ngSanitize',
+    'ui.router',
+    'mgcrea.ngStrap'
 ]);
+
+stoftiApp.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    // Now set up the states
+    $stateProvider
+        .state('home', {
+            url: "/",
+            templateUrl: "/partials/home.html"
+        })
+        .state('about', {
+            url: "/about",
+            templateUrl: "/partials/about.html"
+        })
+        .state('news', {
+            url: "/news",
+            templateUrl: "/partials/news.html"
+        })
+});
