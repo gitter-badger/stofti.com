fb98fa971860e7bde476214c926bbb51f19f863b
Wesley Song <wes.song@gmail.com>
2014-12-16 21:26:40 -0700

    WIP no idea whats changed

--- public/angular/main/maincontroller.js
+++ public/angular/main/maincontroller.js
@@ -1,6 +1,7 @@
 var stoftiApp = angular.module('stoftiApp');
 
 stoftiApp.controller('MainController', ['$scope', '$state', 'Users', function($scope, $state, Users){
+    $scope.isLoggedIn = true;
     $scope.userMenu = [
         {
             "text": "Register",
@@ -18,7 +19,7 @@ stoftiApp.controller('MainController', ['$scope', '$state', 'Users', function($s
             "target": "_self"
         }
     ];
-    $scope.genreMenu = [
+    $scope.readMenu = [
         {
             "text": "Sci-Fi",
             "href": "/read?id=1"
@@ -40,6 +41,29 @@ stoftiApp.controller('MainController', ['$scope', '$state', 'Users', function($s
             "href": "/read?id=3"
         }
     ];
+    $scope.writeMenu = [
+        {
+            "text": "Sci-Fi",
+            "href": "/write?id=1"
+        },
+        {
+            "text": "Romance",
+            "href": "/write?id=2"
+        },
+        {
+            "text": "Fantasy",
+            "href": "/write?id=3"
+        },
+        {
+            "text": "Historical Fiction",
+            "href": "/write?id=3"
+        },
+        {
+            "text": "Miscellaneous",
+            "href": "/write?id=3"
+        }
+    ];
+
     $scope.user = {
         name: "",
         email: "",
